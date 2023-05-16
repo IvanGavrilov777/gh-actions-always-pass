c8c89a731a24dafe6ed6db8ed22ff1166952eefa
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2023-05-16 10:30:19 +0300

    Update init.tf

--- init.tf
+++ init.tf
@@ -7,11 +7,9 @@ terraform {
   }
 }
 
-provider "random" {
-  # Configuration options
-}
-resource "random_pet" {}
+resource "random_pet" "random_pet" {}
+
 output "random_pet_id" {
-  value = random_pet.id
+  value = random_pet.random_pet.id
 }
 
